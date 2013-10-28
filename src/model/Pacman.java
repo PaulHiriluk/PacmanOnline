cc664a52a8372e603aea297887bd60e3ea810f7c
abramovpav <abramovpav@gmail.com>
2013-10-28 18:16:44 +0300

    fast fix

--- src/model/Pacman.java
+++ src/model/Pacman.java
@@ -1,11 +1,17 @@
 package model;
 
+import java.awt.Point;
+
 public class Pacman extends Unit{
+	public Pacman(Point coordinates, UnitType type) {
+
+		super(coordinates, type);
+	}
+
 	private int gold_count;
-	private Boolean life = true;
 	
-	public Boolean isPacmanLife(){
-		return this.life;
+	public int getGold() {
+		return gold_count;
 	}
 	
 	protected void move(){
